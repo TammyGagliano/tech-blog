25a83caa24960006cfcb052f0d071f27982a8cab
Tammy Gagliano <tracsd@gmail.com>
2021-11-22 19:42:35 -0600

    update handlebars files

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,30 +1,26 @@
-async function signupFormHandler(event) {
+const signupFormHandler = async function(event) {
     event.preventDefault();
-
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({
-                username,
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
+  
+    const usernameEl = document.querySelector('#username-input-signup');
+    const passwordEl = document.querySelector('#password-input-signup');
+  
+    const response = await fetch('/api/user', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: usernameEl.value,
+        password: passwordEl.value,
+      }),
+      headers: { 'Content-Type': 'application/json' },
     });
+  
     if (response.ok) {
-        console.log('success');
-
-
-        document.location.replace('/dashboard');
-
+      document.location.replace('/dashboard');
     } else {
-            alert(response.statusText);
+      alert('Failed to sign up');
     }
-    }
-}
-
-document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
+  };
+  
+  document
+    .querySelector('#signup-form')
+    .addEventListener('submit', signupFormHandler);
+  
