7fa8621a0dc90ac5e33e4f7e58d35e71edf20df1
Tammy Gagliano <tracsd@gmail.com>
2021-11-22 20:47:19 -0600

    update seed data

--- seeds/comment.js
+++ seeds/comment.js
@@ -3,27 +3,27 @@ const { Comment } = require('../models');
 const commentData = [
     {
         comment_text: 'Thanks for the information!',
-        user_id: 2, 
-        post_id: 3,
+        userId: 2, 
+        postId: 3,
     },
     {
         comment_text: 'This content is so helpful.',
-        user_id: 4, 
-        post_id: 5,
+        userId: 4, 
+        postId: 5,
     },
     {
         comment_text: 'Why must you post about this?',
-        user_id: 5, 
-        post_id: 1,
+        userId: 5, 
+        postId: 1,
     },
     {
         comment_text: 'Do people even listen to you?',
-        user_id: 3, 
-        post_id: 2,
+        userId: 3, 
+        postId: 2,
     },
     {
         comment_text: 'Great job! Keep up the good work!',
-        user_id: 3, 
+        userId: 3, 
         post_id: 4,
     },
 ];
